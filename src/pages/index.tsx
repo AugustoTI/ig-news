58e918f9ebdeea6b4b080a8adb7a7edacf5f2c30
AugustoTI <augustocesarti2021@gmail.com>
2022-08-19 22:09:46 -0300

    add: adding content from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from './home.module.scss';
 
 const Home: NextPage = () => {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.sectionContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 };
 
 export default Home;
