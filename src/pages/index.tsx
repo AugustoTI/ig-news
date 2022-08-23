8e0bf24d31321c593d4757ecceafaa0f51a0941e
AugustoTI <augustocesarti2021@gmail.com>
2022-08-23 15:03:05 -0300

    chore: adding faunaDB SDK

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps, GetStaticProps, NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
