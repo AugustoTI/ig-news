224a9baef998f80c85341175e3748f607fa6f5e6
AugustoTI <augustocesarti2021@gmail.com>
2022-08-24 19:52:21 -0300

    feat: registering the user logged in to faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { ExprArg, query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,31 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(email as ExprArg)),
+              ),
+            ),
+            q.Create(q.Collection('users'), {
+              data: { email },
+            }),
+            q.Get(
+              q.Match(q.Index('user_by_email'), q.Casefold(email as ExprArg)),
+            ),
+          ),
+        );
+
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+  },
 });
